5b6c67b49fdbccbab1e17532e198e7da9b046367
sparta-project <dnjsdyd78@gmail.com>
2024-07-28 17:51:41 +0900

    level 1 - 여섯 번째 요구사항 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,20 +10,38 @@ public class App {
         int i = -1;
         int count = 0;
 
+
         do {
+            // 결과값들 한칸씩 쉬프트해주는 코드
+            if(i == 9){
+                int j = -1;
+                for(int num: numbers){
+                    if(j == -1){
+                        ++j;
+                        continue;
+                    }
+                    numbers[j] = num;
+                    ++j;
+                    if(j == 9){
+                        --i;
+                        break;
+                    }
+                }
+            }
+            //배열 위치변경 코드
             ++i;
+
+            //연산자와 피연산자의 값을 입력받는 코드
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요: ");
             int num2 = sc.nextInt();
 
             System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
-
             // charAt(index)은 괄호안 문자열의 위치를 받아 해당 데이터의 타입을 char 타입으로 반환하는 메서드
-            // 입력된 값에 따라 변수에 기호값 저장
-
             char operator = sc.next().charAt(0);
 
+            // 입력된 값에 따라 연산과 연산된 값을 변수에 저장해주는 코드
             switch (operator) {
                 case '+':
                     System.out.println("첫 번째 숫자값 : " + num1 + " / " + "두 번째 숫자 값 : " + num2);
@@ -40,7 +58,6 @@ public class App {
                     break;
 
                 case '*':
-                    int result = num1 - num2;
                     System.out.println("첫 번째 숫자값 : " + num1 + " / " + "두 번째 숫자 값 : " + num2);
                     System.out.println("기호값: " + operator);
                     System.out.println("결과값: " + (num1 * num2));
@@ -63,18 +80,19 @@ public class App {
                     System.out.println("입력된 기호값이 올바르지 않습니다.");
             }
 
-
-            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.print("더 계산하시겠습니다? (exit 입력 시 종료)");
             restart = sc.next();
 
             // 반복횟수 카운트
             ++count;
             System.out.println("반복횟수:" + count);
+
         }
+        // 입력값의 조건에 따라 반복문을 끝내는 코드
         while (!restart.equals("exit"));
         System.out.println("계산종료");
 
-        // 저장된 값 출력
+        // 저장된 결과값 출력
         for (int number : numbers) {
             System.out.println(number);
         }
