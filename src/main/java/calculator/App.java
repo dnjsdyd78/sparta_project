359911a93029b01ac64c329ab676570cf166ee8e
sparta-project <dnjsdyd78@gmail.com>
2024-07-28 17:32:11 +0900

    level 1 - 첫 번째 요구사항 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,7 +1,16 @@
 package calculator;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("첫 번째 숫자값 : " + num1 + " / " + "두 번째 숫자 값 : " + num2);
     }
 }
